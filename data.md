47f2d983a01cc647eae10bc1e4fbfcb8cbe5e805
Dr Peter Humburg <peter.humburg@mq.edu.au>
2018-09-25 16:56:35 +1000

    Add weight perception data

--- data.md
+++ data.md
@@ -7,3 +7,6 @@ title: Data downlods
 
 * [Simulated data for 2x2 ANOVA](data/anova_data.csv).
 * [Simulated data for multiple regression](data/regression_data.csv).
+* [Weight perception data](data/weight_perception.csv): Weight (lbs) and height (in)
+  of adolescents between 14 and 20 years old together with a statement describing
+  how they perceive their weight.
---
+++ scripts/weight_height.R
@@ -0,0 +1,32 @@
+library(BayesFactor)
+library(ggplot2)
+
+data <- read.csv("bayesian-data-analysis-in-r/data/weight_perception.csv")
+
+data <- data[!is.na(data$Weight),]
+
+ggplot(data, aes(y=Weight, x=factor(Age), fill=Gender)) + geom_boxplot()
+ggplot(data, aes(x=Height, y=Weight)) + geom_bin2d() + facet_wrap(vars(Gender))
+
+## Full model
+bf_full <- lmBF(Weight ~ Height*Age*Gender, data=data)
+bf_full
+
+## Main effects only
+bf_main <- lmBF(Weight ~ Height+Age+Gender, data=data)
+bf_main
+
+## Strong evidence in favour of full model
+bf_main/bf_full
+
+## Remove age from interaction
+bf_amain <- lmBF(Weight ~ Age + Height*Gender, data=data)
+bf_amain
+
+## Good evidence in favour of simpler model
+bf_amain/bf_full
+
+post_amain <- posterior(bf_amain, iterations = 100000, thin=100)
+summary(post_amain)
+
+plot(post_amain)
